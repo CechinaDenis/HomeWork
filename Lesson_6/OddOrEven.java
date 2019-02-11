bfb0b6d68f24159cfe10da6b5ad7f1938233fd3f
CechinaDenis <Cechina.Denis@gmail.com>
2019-02-11 21:58:07 +0200

    Some minor changes

--- Lesson_6/OddOrEven.java
+++ Lesson_6/OddOrEven.java
@@ -6,10 +6,10 @@ public class OddOrEven{
 		
 		Scanner sc=new Scanner(System.in);
 		
-		System.out.println("\n------= Welcom to 'ODD OR EVEN NUMBER' Application =------\n");
+		System.out.println("\n------= Welcom to 'EVEN OR ODD NUMBER' Application =------\n");
 		System.out.print("Pleas enter the INTEGER NUMBER -> ");
 		int in=sc.nextInt();
 		in %= 2;
-		System.out.println(in==0?"\nYou've entered an ODD Number":"\nYou've entered an EVEN Number");
+		System.out.println(in==0?"\nYou've entered an EVEN Number":"\nYou've entered an ODD Number");
 	}
 }
